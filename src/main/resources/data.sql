488bafb4fecb29eb2ab76a9e442f2846466950bf
unknown <you@example.com>
2021-03-23 15:38:23 +0700

    update

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,9 @@
 INSERT INTO COURSE (code,name,descrition) values('KPDL','Khai pha du lieu','3Tc');
 INSERT INTO COURSE (code,name,descrition) values('KTMT','Kien truc may tinh','3Tc');
+INSERT INTO COURSE (code,name,descrition) values('DTDM','Dien toan dam may','3Tc');
+INSERT INTO COURSE (code,name,descrition) values('TTNT','Tri tue nhan tao','3Tc');
+INSERT INTO COURSE (code,name,descrition) values('LTJ','Lap trinh java','3Tc');
 INSERT INTO STUDENT (name,code_student,address,email) values ('Nam Anh', '1755242', 'Phu Yen', 'abcd123@gmail.com');
 INSERT INTO STUDENT (name,code_student,address,email) values ('Nguyen Tam', '1755242', 'Quy Nhon', 'aaawwwwsss@gmail.com');
+INSERT INTO STUDENT (name,code_student,address,email) values ('Nguyen Dat Anh', '175775242', 'Da Nang', '1q2w3e4r@gmail.com');
+INSERT INTO STUDENT (name,code_student,address,email) values ('Ho Minh', '1755242a', 'Lam Dong', 'awds123@gmail.com');
